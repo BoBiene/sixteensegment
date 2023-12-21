859037e43bc66366c0cde6d1964ec46c19bb4ee4
Bo Biene <23037659+BoBiene@users.noreply.github.com>
2023-12-21 09:43:14 +0100

    keep proportions

--- src/SegmentCanvas.js
+++ src/SegmentCanvas.js
@@ -27,7 +27,7 @@ function SegmentCanvas() {
 
 // Sets the display output to the given text, recalculates 
 // the segment points and draws the segment to the canvas
-SegmentCanvas.prototype.DispayText = function(value) {
+SegmentCanvas.prototype.DispayText = function (value) {
     "use strict";
     // Recalculate points in case any settings changed
     this.CalcPoints();
@@ -37,7 +37,7 @@ SegmentCanvas.prototype.DispayText = function(value) {
 };
 
 // Draws the segment display to a canvas
-SegmentCanvas.prototype.Draw = function(canvas, elements) {
+SegmentCanvas.prototype.Draw = function (canvas, elements) {
     "use strict";
     // Get the context and clear the area
     var context = canvas.getContext('2d');
@@ -64,7 +64,7 @@ SegmentCanvas.prototype.Draw = function(canvas, elements) {
             context.beginPath();
             context.moveTo(this.Points[s][0].x, this.Points[s][0].y);
             // Create the segment path
-            for(var p = 1; p < this.Points[s].length; p++) {
+            for (var p = 1; p < this.Points[s].length; p++) {
                 context.lineTo(this.Points[s][p].x, this.Points[s][p].y);
             }
             context.closePath();
@@ -77,38 +77,42 @@ SegmentCanvas.prototype.Draw = function(canvas, elements) {
 };
 
 // Set the number of elements in the display
-SegmentCanvas.prototype.SetCount = function(count) {
+SegmentCanvas.prototype.SetCount = function (count) {
     "use strict";
     this.ElementArray.SetCount(count);
 };
 
 // Get the number of elements in the display
-SegmentCanvas.prototype.GetCount = function() {
+SegmentCanvas.prototype.GetCount = function () {
     "use strict";
     return this.ElementArray.Elements.length;
 };
 
 // Calculates the width and height of a single display element
 // based on the number of elements and space available in the control
-SegmentCanvas.prototype.CalcElementDimensions = function() {
+SegmentCanvas.prototype.CalcElementDimensions = function () {
     "use strict";
     var n = this.ElementArray.Elements.length;
-    var h = this.Height;
-    h -= this.Padding * 2;
+    
+    var w = Math.min(this.Canvas.width, this.Width);
+    var h = Math.max( this.SegmentWidth * w * 3, Math.min(this.Height, w * 0.4));
 
-    var w = this.Width;
     w -= this.Spacing * (n - 1);
     w -= this.Padding * 2;
     w /= n;
+
+    
+    h -= this.Padding * 2;
+
 
     return { Width: w, Height: h };
 };
 
 // Creates a new set of points flipped vertically
-SegmentCanvas.prototype.FlipVertical = function(points, height) {
+SegmentCanvas.prototype.FlipVertical = function (points, height) {
     "use strict";
     var flipped = [];
-    for(var i=0;i<points.length;i++) {
+    for (var i = 0; i < points.length; i++) {
         flipped[i] = {};
         flipped[i].x = points[i].x;
         flipped[i].y = height - points[i].y;
@@ -117,10 +121,10 @@ SegmentCanvas.prototype.FlipVertical = function(points, height) {
 };
 
 // Creates a new set of points flipped horizontally
-SegmentCanvas.prototype.FlipHorizontal = function(points, width) {
+SegmentCanvas.prototype.FlipHorizontal = function (points, width) {
     "use strict";
     var flipped = [];
-    for(var i=0;i<points.length;i++) {
+    for (var i = 0; i < points.length; i++) {
         flipped[i] = {};
         flipped[i].x = width - points[i].x;
         flipped[i].y = points[i].y;
@@ -129,22 +133,22 @@ SegmentCanvas.prototype.FlipHorizontal = function(points, width) {
 };
 
 // Creates a lighter or darker version of  the color
-SegmentCanvas.GetRGB = function(hex) {
+SegmentCanvas.GetRGB = function (hex) {
     var val = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     var bigint = parseInt(hex, 16);
     var r = (bigint >> 16) & 255;
     var g = (bigint >> 8) & 255;
     var b = bigint & 255;
-    return { R : r, G : g, B : b };
+    return { R: r, G: g, B: b };
 }
 
-SegmentCanvas.Lerp = function(start, end, amount) {
+SegmentCanvas.Lerp = function (start, end, amount) {
     var difference = end - start;
     var adjusted = difference * amount;
     return start + adjusted;
 }
 
-SegmentCanvas.LerpColor = function(colour, colour1, amount) {
+SegmentCanvas.LerpColor = function (colour, colour1, amount) {
     // start colours as lerp-able floats
     var rgb = hexToRgb(colour);
     var rgb1 = hexToRgb(colour1);
@@ -154,9 +158,9 @@ SegmentCanvas.LerpColor = function(colour, colour1, amount) {
     var er = rgb1.R, eg = rgb1.G, eb = rgb1.B;
 
     // lerp the colours to get the difference
-    var  r = SegmentCanvas.Lerp(sr, er, amount),
-         g = SegmentCanvas.Lerp(sg, eg, amount),
-         b = SegmentCanvas.Lerp(sb, eb, amount);
+    var r = SegmentCanvas.Lerp(sr, er, amount),
+        g = SegmentCanvas.Lerp(sg, eg, amount),
+        b = SegmentCanvas.Lerp(sb, eb, amount);
 
     // return the new colour
     return rgbToHex(r, g, b);
@@ -165,7 +169,7 @@ SegmentCanvas.LerpColor = function(colour, colour1, amount) {
 function hexToRgb(hex) {
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
         return r + r + g + g + b + b;
     });
 
